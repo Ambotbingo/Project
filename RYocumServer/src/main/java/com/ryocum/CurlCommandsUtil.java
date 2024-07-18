f1c9b3caa8251d70db767eb6352839c219fee997
Ambotbingo <74734699+Ambotbingo@users.noreply.github.com>
2024-07-18 16:27:38 -0700

    Update CurlCommandsUtil.java

--- RYocumServer/src/main/java/com/ryocum/CurlCommandsUtil.java
+++ RYocumServer/src/main/java/com/ryocum/CurlCommandsUtil.java
@@ -54,7 +54,7 @@ public final class CurlCommandsUtil {
 
                     jsonResp = gson.toJson(status.getState());
                 }
-                jsonResp = gson.toJson(status.getState());
+                //return newFixedLengthResponse("Empty Status");
             }
 
             return newFixedLengthResponse(jsonResp);
